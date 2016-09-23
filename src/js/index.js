5ddfea8dc97e9a28497ce09be211f08df198c2ce
Valentin Drany <navshop@ukr.net>
2016-09-23 13:33:15 +0300

    del some proporty

--- src/js/index.js
+++ src/js/index.js
@@ -4,86 +4,129 @@
 
 	console.log("file 'index.js' work!!! ")
 
-	var step = 0.5,
-		currentLeft = [],
-		i = 0
+	var step = 0.5,					//Скорость анимации
+		currentLeft = [],			//Текущее положение картинок
+		startPositionLeft = 0,		//Начальная позиция картинок
+		timeAnim = 0,				//Время анимации
+		widthImgs = 0,				//Ширина всех картинок
+		carouselMove = false,  		//Движение анимации
+		initialImgs = []			//Начальное кол-во картинок
 	
 	var $carousel = document.querySelector('.carousel')
-	var $img = $carousel.querySelector('img')
-	var $imgCloneFirst = $img.cloneNode()
-	var $imgCloneSecond = $img.nextElementSibling.cloneNode()
-
-	$carousel.appendChild($imgCloneFirst)
-	$carousel.appendChild($imgCloneSecond)
-	
 	var $imgs = $carousel.querySelectorAll('img')
 
-	var startPositionLeft = 0
-	var imgWidth = $img.clientWidth
-	$imgs.forEach(function($el, index)
+	$imgs.forEach(function($el, index) //Определяем макс длину картинок и начальные картинки сохраняем
 	{
-		$el.style.left = -$el.offsetLeft + startPositionLeft + 'px'
-		currentLeft[index] = startPositionLeft
-		startPositionLeft += imgWidth
-		console.log($el.style.left)
+		widthImgs += $el.clientWidth
+		initialImgs[index] = $el.cloneNode()
 	})
 
-
-	var i = 0
-	var requestAnimationFrameCarousel = requestAnimationFrame(doAnim)
-
-	function doAnim()
+	function move_carousel() //Запускает анимацию
 	{
-		if ( $img.offsetLeft <= -$img.clientWidth )
-		{
-			for (var i = 0; i < currentLeft.length-1; i++) {
-			 	currentLeft[i] = i * imgWidth
-			 } 
-
-			//currentLeft = 0
-			startPositionLeft = 0
-			$img = $carousel.removeChild($carousel.firstElementChild)
- 		 	$img = $carousel.querySelector('img')
- 		// 	$imgCloneFirst = $img.cloneNode()
-			// $imgCloneSecond = $img.nextElementSibling.cloneNode()
-
-// 			$carousel.appendChild($imgCloneFirst)
-
-// 			//$carousel.firstElementChild.style.left = -$carousel.firstElementChild.offsetLeft + 'px'
- 			$imgs = $carousel.querySelectorAll('img')
-// 			$imgs.forEach(function($el)
-// 			{
-// 		//		console.log('До: Left - ' + $el.style.left + '; Offset - ' + $el.offsetLeft)
-// 				$el.style.left = startPositionLeft + 'px'
-// 				startPositionLeft += imgWidth
-// 		//		console.log('После: Left - ' + $el.style.left + '; Offset - ' + $el.offsetLeft)
-// 			})
-// //			console.log('-----')
+		// добавляем две первые картинку в конец
+		var $img = $carousel.querySelector('img')
+		var $imgCloneFirst = $img.cloneNode()
+		var $imgCloneSecond = $img.nextElementSibling.cloneNode()
+		$carousel.appendChild($imgCloneFirst)
+		$carousel.appendChild($imgCloneSecond)
 		
-		}
+		$imgs = $carousel.querySelectorAll('img')
 
-
-		if ( i > 500 )
-		{
-			cancelAnimationFrame(requestAnimationFrameCarousel)
-			return
-		}
-
-		console.log('-----')
+		// выставляем позицию картинок по левому краю
 		$imgs.forEach(function($el, index)
 		{
-			$el.style.left = currentLeft[index] + 'px'
-			currentLeft[index] -= step
-			console.log($el.style.left)
+			$el.style.left = startPositionLeft + 'px'
+			currentLeft[index] = startPositionLeft
 		})
 
-		i++
+		// Анимация
+		var requestAnimationFrameCarousel = requestAnimationFrame(doAnim)
+		function doAnim()
+		{
+			if ( $img.offsetLeft <= -$img.clientWidth )
+			{
+				for (var i = 0; i < currentLeft.length; i++) 
+				{
+				 	currentLeft[i] = startPositionLeft
+				} 
 
-		requestAnimationFrameCarousel = requestAnimationFrame(doAnim)
+		// Вставляем первый элемент в конец
+				$imgCloneFirst = $img.cloneNode()
+				$imgCloneFirst.style.left = startPositionLeft + 'px' 
+				$carousel.appendChild($imgCloneFirst)
 
+		// Удаляем первый элемент
+				$img = $carousel.removeChild($carousel.firstElementChild)
+	 		 	$img = $carousel.querySelector('img')
+
+	 	// выстраиваем элементы заново	 		
+				$imgs = $carousel.querySelectorAll('img')
+				$imgs.forEach(function($el)
+				{
+					$el.style.left = startPositionLeft + 'px'
+				})
+			}
+
+			if (  widthImgs < $carousel.clientWidth )
+			{
+				cancelAnimationFrame(requestAnimationFrameCarousel)
+				return
+			}
+
+			$imgs.forEach(function($el, index)
+			{
+				currentLeft[index] -= step
+				$el.style.left = currentLeft[index] + 'px'
+			})
+
+			timeAnim++
+
+			requestAnimationFrameCarousel = requestAnimationFrame(doAnim)
+		}
 	}
 
+	function check_relevance_carousel() //Проверяем на необходимость движения анимации
+	{
+		if ( widthImgs > $carousel.clientWidth )
+		{
+			$carousel.style.justifyContent = 'flex-start'
 
+
+			if (!carouselMove)
+			{
+				move_carousel()
+				carouselMove = true
+			}
+		}
+		else
+		{
+			if (carouselMove)
+			{
+				$imgs = $carousel.querySelectorAll('img')
+				$imgs.forEach(function($el)
+				{
+					$carousel.removeChild($el)
+				})
+
+				for (var i = 0; i < initialImgs.length; i++)
+				{
+					initialImgs[i].style.left = startPositionLeft
+					$carousel.appendChild(initialImgs[i])
+				}
+
+				carouselMove = false
+			}
+
+			$carousel.style.justifyContent = 'space-around'
+		}
+	}
+
+	check_relevance_carousel()
+
+	window.addEventListener('resize', function()
+	{
+		check_relevance_carousel()
+	})
 
 })()
 
